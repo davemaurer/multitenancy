0ade3fad774b27eed394d562a520a241dcd7ceba
David Maurer <davemaurermail@gmail.com>
2015-07-06 10:24:45 -0600

    hotseat work

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216235605) do
+ActiveRecord::Schema.define(version: 20150706160405) do
 
   create_table "items", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "merchants", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
